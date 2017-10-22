e867159c354077c31799397b6ac26f392d8e1276
Ken Rutan <kenr16@gmail.com>
2017-10-21 22:51:29 -0700

    added framework for basic api call

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -4,7 +4,8 @@ class MembersController < ApplicationController
   # GET /members
   # GET /members.json
   def index
-    @members = Member.all
+    @members = Member.response
+  #  @members = Member.all
   end
 
   # GET /members/1
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,18 @@
 class Member < ApplicationRecord
+  include HTTParty
+
+  def self.response
+    auth = {
+      username: "...",
+      password: "..."
+    }
+
+    options = { basic_auth: auth, headers: {
+    "X-API-KEY" => "api-key-here"
+    } }
+
+    data_url = "https://..."
+
+    HTTParty.get(data_url, options)
+  end
 end
